64a59bc570a412f3622ec7ee8809e375909489a6
Aimee Rivers <drexairi@dr.dk>
2018-10-10 14:36:32 +0200

    Muligheden for testing hos Safari.

--- features/support/env.rb
+++ features/support/env.rb
@@ -8,10 +8,12 @@ require 'rspec'
 Capybara.app_host = 'https://www.dr.dk'
 
 case ENV['TEST_ENV']
-when 'chrome'
-  Capybara.default_driver = :selenium_grid_chrome
 when 'firefox'
   Capybara.default_driver = :selenium_grid_firefox
+when 'chrome'
+  Capybara.default_driver = :selenium_grid_chrome
+when 'safari'
+  Capybara.default_driver = :selenium_grid_safari
 else
   Capybara.default_driver = :selenium_grid_firefox
 end
@@ -34,10 +36,21 @@ Capybara.register_driver :selenium_grid_chrome do |app|
     :desired_capabilities => Selenium::WebDriver::Remote::Capabilities.chrome)
 end
 
+Capybara.register_driver :selenium_grid_safari do |app|
+  Capybara::Selenium::Driver.new(app,
+    :browser => :remote,
+    :url => selenium_url,
+    :desired_capabilities => Selenium::WebDriver::Remote::Capabilities.safari)
+end
+
 Retriable.configure do |c|
   c.on = RSpec::Expectations::ExpectationNotMetError
   c.base_interval = 0.1
   c.tries = 10
+end
+
+Before do
+  page.driver.browser.manage.window.resize_to(1280, 1024)
 end
 
 After do |scenario|
