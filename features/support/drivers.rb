01fb63d14729c7de905a8c2b895b42c0460f6ee3
Aimee Rivers <drexairi@dr.dk>
2018-10-18 11:41:29 +0200

    Konfiguration til flere browsers.

--- features/support/drivers.rb
+++ features/support/drivers.rb
@@ -1,32 +1,21 @@
-selenium_hub_addr = ENV['SELENIUM_HUB_ADDR'] || 'selenium_hub'
-selenium_hub_port = ENV['SELENIUM_HUB_PORT'] || '4444'
+selenium_hub_addr = ENV.fetch('SELENIUM_HUB_ADDR', 'selenium_hub')
+selenium_hub_port = ENV.fetch('SELENIUM_HUB_PORT', '4444')
 selenium_url = "http://#{selenium_hub_addr}:#{selenium_hub_port}/wd/hub"
 
-drivers = {
-  selenium_grid_firefox: Selenium::WebDriver::Remote::Capabilities.firefox,
-  selenium_grid_chrome: Selenium::WebDriver::Remote::Capabilities.chrome,
-  selenium_grid_safari: Selenium::WebDriver::Remote::Capabilities.safari,
-  selenium_grid_ie: Selenium::WebDriver::Remote::Capabilities.internet_explorer
-}
+Capybara.register_driver :selenium_grid do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.new
+  capabilities.browser_name = ENV.fetch('BROWSER', 'firefox')
+  capabilities.platform = ENV.fetch('PLATFORM', :any)
+  capabilities.javascript_enabled = true
+  capabilities.takes_screenshot = true
+  capabilities['ie.ensureCleanSession'] = true
 
-drivers.each do |key, driver|
-  Capybara.register_driver key do |app|
-    Capybara::Selenium::Driver.new(app,
-      :browser => :remote,
-      :url => selenium_url,
-      :desired_capabilities => driver)
-  end
+  driver_options = {
+    browser: :remote,
+    url: selenium_url,
+    desired_capabilities: capabilities,
+  }
+  Capybara::Selenium::Driver.new app, driver_options
 end
 
-case ENV['TEST_ENV']
-when 'firefox'
-  Capybara.default_driver = :selenium_grid_firefox
-when 'chrome'
-  Capybara.default_driver = :selenium_grid_chrome
-when 'safari'
-  Capybara.default_driver = :selenium_grid_safari
-when 'ie'
-  Capybara.default_driver = :selenium_grid_ie
-else
-  Capybara.default_driver = :selenium_grid_firefox
-end
+Capybara.default_driver = :selenium_grid
