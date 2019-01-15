8489fcf22cfc2f5297434772fabf07b9819041fd
Aimee Rivers <airi@dr.dk>
2019-01-15 17:11:47 +0100

    Brug net-http-persistent.

--- features/support/drivers.rb
+++ features/support/drivers.rb
@@ -1,3 +1,5 @@
+require 'selenium/webdriver/remote/http/persistent'
+
 selenium_hub_addr = ENV.fetch('SELENIUM_HUB_ADDR', 'selenium_hub')
 selenium_hub_port = ENV.fetch('SELENIUM_HUB_PORT', '4444')
 selenium_url = "http://#{selenium_hub_addr}:#{selenium_hub_port}/wd/hub"
@@ -10,10 +12,13 @@ Capybara.register_driver :selenium_grid do |app|
   capabilities.takes_screenshot = true
   capabilities['ie.ensureCleanSession'] = true
 
+  client = Selenium::WebDriver::Remote::Http::Persistent.new
+
   driver_options = {
     browser: :remote,
     url: selenium_url,
     desired_capabilities: capabilities,
+    http_client: client
   }
   Capybara::Selenium::Driver.new app, driver_options
 end
