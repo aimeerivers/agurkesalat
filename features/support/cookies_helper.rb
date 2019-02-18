908f2a7aacd4a1d5475b122f6a8909da6bd6e8f5
Aimee Rivers <airi@dr.dk>
2019-02-18 13:05:52 +0100

    Bedre handling af cookie dialog.

--- features/support/cookies_helper.rb
+++ features/support/cookies_helper.rb
@@ -1,11 +1,16 @@
 module CookiesHelper
 
-  def accept_cookies
-    sleep 0.5
-    cookie_buttons = page.all('#CybotCookiebotDialogBodyButtonAccept')
-    if cookie_buttons.any?
-      cookie_buttons.first.click
+  def accept_cookies(options = {})
+    if options[:explicit_click]
       sleep 0.5
+      cookie_buttons = page.all('#CybotCookiebotDialogBodyButtonAccept')
+      if cookie_buttons.any?
+        cookie_buttons.first.click
+        sleep 0.5
+        page.driver.browser.navigate.refresh
+      end
+    else
+      page.execute_script(%Q{document.cookie = "CookieConsent={stamp:'CuZcRMETDBzzXrvOSRUm3NdONNifYwwAIxGSfRITWhTTtCfsa56iuw=='%2Cnecessary:true%2Cpreferences:true%2Cstatistics:true%2Cmarketing:true%2Cver:2}";})
       page.driver.browser.navigate.refresh
     end
   end
